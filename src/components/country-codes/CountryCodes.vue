a40db174c1b5a6b934252c4faec4f2c55f700042
Igor Mironets <i.mironets@gmail.com>
2019-10-04 17:11:31 +0300

    arrow keys navigation

--- src/components/country-codes/CountryCodes.vue
+++ src/components/country-codes/CountryCodes.vue
@@ -14,7 +14,7 @@
       <div class="CountryCodes-Dropdown-Item" 
            v-for="(country, index) in countries"
            :key="country.iso2"
-           @click.prevent="onCountrySelect(country)"
+           @click.prevent="onCountrySelect(country, index)"
            v-bind:class="{'_highlighted': index === highlightedIndex, '_selected': index === selectedIndex }"
       >{{country.name}}</div>
     </div>
@@ -50,8 +50,8 @@ export default class CountryCodes extends Vue {
   public countries: Country[] = [];
   public selectedCode: string = '';
   public dropDownIsShown: boolean = false;
-  private handleOutsideClick: (e: any) => void;
 
+  private handleOutsideClick: (e: any) => void;
   private selectedIndex: number = null;
   private highlightedIndex: number = null;
   private itemHeight: number = 28;
@@ -71,7 +71,8 @@ export default class CountryCodes extends Vue {
     this._selectDefaultCountry();
   }
 
-  public onCountrySelect(country: Country) {
+  public onCountrySelect(country: Country, index: number) {
+    this.selectedIndex = index;
     this.selectedCode = `+${country.dialCode}`;
     this.closeDropdown();
   }
@@ -101,12 +102,34 @@ export default class CountryCodes extends Vue {
       this._selectByIndex(this.highlightedIndex);
       return false;
     }
-    // On Up|Down Arrow keys
-    if (e.keyCode === 40 || e.keyCode === 38) {
-      //
+    // On Down Arrow key
+    if (e.keyCode === 40) {
+      if (this.highlightedIndex === null && this.selectedIndex !== null) {
+        this.highlightedIndex = this.selectedIndex;
+      }
+      if (this.highlightedIndex < this.countries.length - 1) {
+        this.highlightedIndex ++;
+      } else {
+        this.highlightedIndex = 0;
+      }
+      this._scrollToIndex(this.highlightedIndex);
+      return false;
+    }
+    // On Up Arrow key
+    if (e.keyCode === 38) {
+      if (this.highlightedIndex === null && this.selectedIndex !== null) {
+        this.highlightedIndex = this.selectedIndex;
+      }
+      if (this.highlightedIndex !== 0) {
+        this.highlightedIndex --;
+      } else {
+        this.highlightedIndex = this.countries.length - 1;
+      }
+      this._scrollToIndex(this.highlightedIndex);
+      return false;
     }
 
-    console.log(e.keyCode);
+    // On Other keys
     this._searckByKeyPress(e.key);
   }
 
@@ -172,8 +195,7 @@ export default class CountryCodes extends Vue {
     if (index !== null) {
       const country = this.countries[index];
       if (country) {
-        this.selectedIndex = index;
-        this.onCountrySelect(country);
+        this.onCountrySelect(country, index);
       }
     }
   }
@@ -181,8 +203,7 @@ export default class CountryCodes extends Vue {
   private _selectDefaultCountry() {
     const match = this._findFirstMatch(this.defaultCountryIso2, 'iso2', true);
     if (match.item) {
-      this.selectedIndex = match.index;
-      this.onCountrySelect(match.item);
+      this.onCountrySelect(match.item, match.index);
     }
   }
 
